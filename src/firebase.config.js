dffa43e47304065cb0679a62f7b8e25daaf08af4
Krunal Dave <krunal.d@upsquare.in>
2024-07-30 17:49:01 +0530

    Firebase setting changes

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,15 +7,23 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA8hUZ2zNnsKi8JFiifXKR6lMTqHlhx4y8",
-  authDomain: "ooooo-5419a.firebaseapp.com",
-  projectId: "ooooo-5419a",
-  storageBucket: "ooooo-5419a.appspot.com",
-  messagingSenderId: "703873231799",
-  appId: "1:703873231799:web:81a899e16f8a1874ff5632"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA8hUZ2zNnsKi8JFiifXKR6lMTqHlhx4y8",
+//   authDomain: "ooooo-5419a.firebaseapp.com",
+//   projectId: "ooooo-5419a",
+//   storageBucket: "ooooo-5419a.appspot.com",
+//   messagingSenderId: "703873231799",
+//   appId: "1:703873231799:web:81a899e16f8a1874ff5632"
+// };
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDeGYvqap5QglWuqn4mOeB3YqR8xuGRUZs",
+  authDomain: "testing-otp-ecb1f.firebaseapp.com",
+  projectId: "testing-otp-ecb1f",
+  storageBucket: "testing-otp-ecb1f.appspot.com",
+  messagingSenderId: "228004346713",
+  appId: "1:228004346713:web:774bcb23b3b014a986aa48"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
